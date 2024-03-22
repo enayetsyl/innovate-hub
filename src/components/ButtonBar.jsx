ebadce983e5fb6e156acf401696fc54080fab648
Enayet <enayetflweb@gamil.com>
2024-03-22 20:46:55 +0600

    Button bar comment done

--- src/components/ButtonBar.jsx
+++ src/components/ButtonBar.jsx
@@ -4,12 +4,14 @@ import { toast } from "react-toastify"
 
 const ButtonBar = () => {
   return (
+    // Container
     <div className="p-5 flex justify-between items-center ">
       
       {/* Left side button */}
     <div className="flex justify-center items-center gap-3">
       {
         dropdownButtonInfo.map(item => (
+          // First three button
           <button key={item.name}
           className="flex justify-center items-center gap-1 border border-zinc-400 p-2 rounded-xl font-semibold shadow-lg hover:-translate-y-1 transition-all duration-300"
           onClick={()=> toast(`Imagine dropdown list appeared.`)}
@@ -21,6 +23,7 @@ const ButtonBar = () => {
         ))
       }
       {
+        // Middle two button
         dashedButtonInfo.map(item => (
           <button key={item.name}
           className="flex justify-center items-center gap-1 border border-dashed border-zinc-400 p-2 rounded-xl font-semibold text-gray-500 bg-gray-300 hover:-translate-y-1 transition-all duration-300"
